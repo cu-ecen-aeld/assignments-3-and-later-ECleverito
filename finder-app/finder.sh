2b01715a71fc691739c5071adaa1ad16cb7cf1a2
ercl9347 <ercl9347@colorado.edu>
2022-08-29 15:31:25 -0600

    Completed finder.sh portion

---
+++ finder-app/finder.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+if [ $# -lt 2 ]
+then
+	echo "Insufficient number of arguments provided"
+	exit 1
+fi
+
+filesdir=$1
+searchstr=$2
+
+if [ ! -d "$filesdir" ]
+then
+	echo "$filesdir does not exist"	
+	exit 1
+fi
+
+numFiles=$(ls -Rl "$filesdir" | wc -l)
+numStr=$(grep -r "$searchstr" "$filesdir" | wc -l)
+
+echo "The number of files are $numFiles and the number of matching lines\
+	are $numStr"
+
+exit 0

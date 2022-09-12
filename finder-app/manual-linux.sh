aa48e304e81868906c600b8a3bd9ca587e413a21
ECleverito <eclever12@gmail.com>
2022-09-11 22:34:16 -0600

    Successful emulation!

--- finder-app/manual-linux.sh
+++ finder-app/manual-linux.sh
@@ -34,7 +34,6 @@ if [ ! -e ${OUTDIR}/linux-stable/arch/${ARCH}/boot/Image ]; then
     echo "Checking out version ${KERNEL_VERSION}"
     git checkout ${KERNEL_VERSION}
 
-    # TODO: Add your kernel build steps here
     make ARCH=${ARCH} CROSS_COMPILE=${CROSS_COMPILE} mrproper
     make ARCH=${ARCH} CROSS_COMPILE=${CROSS_COMPILE} defconfig
     make -j4 ARCH=${ARCH} CROSS_COMPILE=${CROSS_COMPILE} all
@@ -54,13 +53,14 @@ then
     sudo rm  -rf ${OUTDIR}/rootfs
 fi
 
-# TODO: Create necessary base directories
+echo "Creating root filesystem base directories"
 mkdir ${OUTDIR}/rootfs
 cd ${OUTDIR}/rootfs
 mkdir bin dev etc home lib proc sbin sys tmp usr var
 mkdir usr/bin usr/lob usr/sbin
 mkdir -p var/log
 
+#Setting ownership of entire rootfs to root
 sudo chown -R root:root *
 
 cd "$OUTDIR"
@@ -69,14 +69,14 @@ then
 git clone git://busybox.net/busybox.git
     cd busybox
     git checkout ${BUSYBOX_VERSION}
-    # TODO:  Configure busybox
+    #Configure busybox
     make distclean
     make defconfig
 else
     cd busybox
 fi
 
-# TODO: Make and install busybox
+#Make and install busybox
 sudo "PATH=$PATH" make ARCH=${ARCH} CROSS_COMPILE=${CROSS_COMPILE} CONFIG_PREFIX=${OUTDIR}/rootfs install
 
 cd ${OUTDIR}/rootfs
@@ -85,41 +85,38 @@ echo "Library dependencies"
 ${CROSS_COMPILE}readelf -a bin/busybox | grep "program interpreter"
 ${CROSS_COMPILE}readelf -a bin/busybox | grep "Shared library"
 
-# TODO: Add library dependencies to rootfs
+#Add library dependencies to rootfs
 SYSROOT=$(${CROSS_COMPILE}gcc -print-sysroot)
 
 cd ${OUTDIR}/rootfs
-sudo cp -a /${SYSROOT}/lib/ld-linux-aarch64.so.1 lib
-sudo cp -a /${SYSROOT}/lib64/ld-2.31.so lib
-sudo cp -a /${SYSROOT}/lib64/libm.so.6 lib
-sudo cp -a /${SYSROOT}/lib64/libm-2.31.so lib
-sudo cp -a /${SYSROOT}/lib64/libresolv.so.2 lib
-sudo cp -a /${SYSROOT}/lib64/libresolv-2.31.so lib
-sudo cp -a /${SYSROOT}/lib64/libc.so.6 lib
-sudo cp -a /${SYSROOT}/lib64/libc-2.31.so lib
+sudo mkdir lib64/
+sudo cp -a /${SYSROOT}/lib/ld-linux-aarch64.so.1 lib/
+sudo cp -a /${SYSROOT}/lib64/ld-2.31.so lib64/
+sudo cp -a /${SYSROOT}/lib64/libm.so.6 lib64/
+sudo cp -a /${SYSROOT}/lib64/libm-2.31.so lib64/
+sudo cp -a /${SYSROOT}/lib64/libresolv.so.2 lib64/
+sudo cp -a /${SYSROOT}/lib64/libresolv-2.31.so lib64/
+sudo cp -a /${SYSROOT}/lib64/libc.so.6 lib64/
+sudo cp -a /${SYSROOT}/lib64/libc-2.31.so lib64/
 
-# TODO: Make device nodes
+#Make device nodes
 cd ${OUTDIR}/rootfs
 sudo mknod -m 666 dev/null c 1 3
 sudo mknod -m 600 dev/console c 5 1
 
-sudo cp -r ~/AESD/assignments/3/assignment-3-ECleverito/finder-app ${OUTDIR}/rootfs/home
-
-cd ${OUTDIR}/rootfs/home/finder-app
+cd ${FINDER_APP_DIR}
 make clean
 sudo "PATH=$PATH" make CC=${CROSS_COMPILE}gcc writer
 
-# TODO: Clean and build the writer utility
-
-
 # TODO: Copy the finder related scripts and executables to the /home directory
 # on the target rootfs
-
-# TODO: Chown the root directory
-
+sudo cp writer ${OUTDIR}/rootfs/home
+sudo cp finder.sh ${OUTDIR}/rootfs/home
+sudo cp -r ../conf ${OUTDIR}/rootfs/home
+sudo cp finder-test.sh ${OUTDIR}/rootfs/home 
+sudo cp autorun-qemu.sh ${OUTDIR}/rootfs/home
 
 # TODO: Create initramfs.cpio.gz
-cd ${OUTDIR}/rootfs
 find . | cpio -H newc -ov --owner root:root > ../initramfs.cpio
 cd ..
 gzip initramfs.cpio

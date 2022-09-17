f781370752a16471824af0da5f491323873fc82b
ECleverito <eclever12@gmail.com>
2022-09-17 15:37:37 -0600

    Solved attachment issues with created thread

--- examples/threading/threading.c
+++ examples/threading/threading.c
@@ -3,6 +3,9 @@
 #include <stdlib.h>
 #include <stdio.h>
 
+#include <string.h>
+#include <errno.h>
+
 // Optional: use these functions to add debug or error prints to your application
 #define DEBUG_LOG(msg,...)
 //#define DEBUG_LOG(msg,...) printf("threading: " msg "\n" , ##__VA_ARGS__)
@@ -10,11 +13,35 @@
 
 void* threadfunc(void* thread_param)
 {
+    struct thread_data* thread_func_args = (struct thread_data *) thread_param;
 
-    // TODO: wait, obtain mutex, wait, release mutex as described by thread_data structure
-    // hint: use a cast like the one below to obtain thread arguments from your parameter
-    //struct thread_data* thread_func_args = (struct thread_data *) thread_param;
-    return thread_param;
+    thread_func_args->thread_complete_success = false;
+
+    usleep(thread_func_args->wait_to_obtain_ms*1000);
+    
+    int lockReturnVal = 0;
+
+    lockReturnVal = pthread_mutex_lock(thread_func_args->mutex);
+
+    if(lockReturnVal!=0)
+    {
+        perror(strerror(errno));
+        pthread_exit(thread_param);
+    }
+
+    usleep(thread_func_args->wait_to_release_ms*1000);
+
+    lockReturnVal = pthread_mutex_unlock(thread_func_args->mutex);
+
+    if(lockReturnVal!=0)
+    {
+        perror(strerror(errno));
+        pthread_exit(thread_param);
+    }
+
+    thread_func_args->thread_complete_success = true;
+
+    pthread_exit(thread_param);
 }
 
 
@@ -28,6 +55,28 @@ bool start_thread_obtaining_mutex(pthread_t *thread, pthread_mutex_t *mutex,int 
      *
      * See implementation details in threading.h file comment block
      */
-    return false;
+    struct thread_data *my_thread_data = (struct thread_data *) malloc(sizeof(struct thread_data));
+
+    //Check if memory is still available for allocation
+    if(my_thread_data == NULL)
+    {
+        return false;
+    }
+
+    my_thread_data->mutex = mutex;
+    my_thread_data->wait_to_obtain_ms = wait_to_obtain_ms;
+    my_thread_data->wait_to_release_ms = wait_to_release_ms;
+
+    if(pthread_create(thread, NULL, threadfunc, my_thread_data) != 0)
+    {
+        ERROR_LOG("Thread could not be created");
+        return false;
+    }
+    else
+    {
+        // pthread_detach(*thread);
+        return true;
+    }
+
 }
 

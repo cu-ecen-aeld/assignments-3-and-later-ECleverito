d1cee3d5ce3b16bbf66ee58ab99f342c4d1604a6
ECleverito <eclever12@gmail.com>
2022-09-30 01:50:01 -0600

    Writing out to output file from netcat input, but still need to reorganize functions and return file to calling peer

--- server/aesdsocket.c
+++ server/aesdsocket.c
@@ -1,12 +1,17 @@
 
+#include "aesdsocket.h"
+
 #include <stdio.h>
 #include <errno.h>
 #include <string.h>
 
 #include <sys/types.h>
 #include <sys/socket.h>
+#include <sys/stat.h>
 #include <netdb.h>
 #include <syslog.h>
+#include <fcntl.h>
+#include <unistd.h>
 
 int main()
 {
@@ -45,7 +50,7 @@ int main()
     socklen_t peer_addr_size = sizeof(peeraddr);
     
     int connectedSock = accept(sockfd, &peeraddr, &peer_addr_size);
-    if(connectedSock!=-1)
+    if(connectedSock==-1)
     {
         perror("listen() error");
         return -1;
@@ -53,7 +58,40 @@ int main()
 
     openlog("aesdsocket", 0, LOG_USER);
 
-    syslog(LOG_INFO, "Connection established!");
+    syslog(LOG_INFO, "Accepted connection from %s", peeraddr.sa_data);
+
+    int fd = creat(OUTPUT_FILEPATH, S_IRUSR | S_IWUSR | S_IRGRP | S_IWGRP);
+
+    if(fd==-1)
+    {
+        perror("creat() error");
+        return -1;
+    }
+
+    //Receive a single byte at a time
+    char recvdByte;
+    int totalByteCnt, pktByteCnt = 0;
+
+    while(recv(connectedSock, &recvdByte, 1, 0)!=0)
+    {
+        
+        while(write(fd, &recvdByte, 1)!=1)
+            ;
+
+        pktByteCnt++;
+        totalByteCnt++;
+
+        //Output contents of output file to
+        //the peer for every packet received
+        if(recvdByte=='\n')
+        {
+            pktByteCnt=0;
+            //COMPLETE LATER
+        }
+        
+    }
+
+    syslog(LOG_INFO, "Closed connection from %s", peeraddr.sa_data);
 
     closelog();
 
---
+++ server/aesdsocket.h
@@ -0,0 +1,7 @@
+#pragma once
+
+#define BACKLOG     1
+#define PORT        9000
+#define BUFF_SIZE   256
+
+const char OUTPUT_FILEPATH[] = "/var/tmp/aesdsocketdata";

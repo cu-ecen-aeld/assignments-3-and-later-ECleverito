b3cce8879a9b8fc5d459cce39add01831a4fbdfe
ECleverito <eclever12@gmail.com>
2022-10-24 21:56:44 -0600

    I was taking too much in the copy to user before

--- aesd-char-driver/main.c
+++ aesd-char-driver/main.c
@@ -76,20 +76,20 @@ ssize_t aesd_read(struct file *filp, char __user *buf, size_t count,
 
     if(count>(offsetEntry->size - offsetEntry_ind))
     {
-        bytesLeft = offsetEntry->size - offsetEntry_ind;
+        newCount = offsetEntry->size - offsetEntry_ind;
     }
     else
     {
-        bytesLeft = count;
+        newCount = count;
     }
 
-    newCount = bytesLeft;
-
+    bytesLeft=newCount;
     do{
+
         bytesLeft = copy_to_user(buf+newCount-bytesLeft,
                                 &(offsetEntry->buffptr[offsetEntry_ind+newCount-bytesLeft]),
                                 bytesLeft);
-        newCount -= bytesLeft;
+        
 
     } while(bytesLeft != 0);
 
